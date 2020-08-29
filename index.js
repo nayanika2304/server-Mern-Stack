443ecec5abbc0b602512a10541550f9ee6188066
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-08-29 16:36:35 -0400

    google OAuth setup

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
-app.get('/',(req,res) =>{
-    res.send({hi: 'I changed it'});
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret : keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+    },
+    (accessToken,refreshToken,profile,done) =>{
+    console.log(accessToken)
 })
+);
 
+/*
+Here passport.authenticate checks for the string 'google' and goes to the above declared strategy to use the ifo provided
+ */
+app.get('/auth/google',passport.authenticate(
+    'google',
+    {scope: ['profile','email']},
+    )
+);
+
+app.get('/auth/google/callback',passport.authenticate(
+    'google',
+
+))
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
+
