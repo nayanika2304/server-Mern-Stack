074f4b8f4faaf46ba408d13914c787c48767a7bf
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-09-01 16:52:49 -0400

    finished auth flow

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-require('./services/passport')
+
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+
+require('./models/user')
+require('./services/passport')
+
 const app = express();
+
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000 ,
+    keys: [keys.cookieKey]
+}))
+
+app.use(passport.initialize());
+app.use(passport.session())
 
 mongoose
     .connect(
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,20 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users')
+
+passport.serializeUser((user,done) =>{
+    done(null,user.id)
+})
+
+passport.deserializeUser((id,done) =>{
+    User.findById(id)
+        .then(user => {
+            done(null,user)
+        })
+})
 
 passport.use(new GoogleStrategy({
         clientID: keys.googleClientID,
@@ -8,7 +22,16 @@ passport.use(new GoogleStrategy({
         callbackURL: '/auth/google/callback'
     },
     (accessToken,refreshToken,profile,done) =>{
-        console.log(accessToken)
+        User.findOne({goggleId: profile.id})
+            .then((existingUser) =>{
+                if(existingUser){
+                    done(null,existingUser);
+                }else{
+                    new User({googleId: profile.id}).save()
+                        .then(user => done(null,user))
+                }
+            })
+
     })
 );
 
