d43b19e9f65e54cbaafb79d19a8c26f92313d93d
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-09-06 15:03:38 -0400

    header, landing and getting started with stripe checkout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,32 @@
 import React,{Component} from 'react';
+import {connect} from "react-redux";
+import {Link} from 'react-router-dom';
 
 class Header extends Component{
+    renderContent(){
+        switch(this.props.auth){
+            case null:
+                return;
+            case false:
+                return (
+                    <li><a href="/auth/google">Login with Google</a></li>
+                )
+            default:
+                return(
+                    <li><a href="/api/logout">Logout</a></li>
+                )
+        }
+    }
+
     render(){
         return(
             <nav>
                 <div className="nav-wrapper">
-                    <a href="/" className="left brand-logo">Emaily</a>
+                    <Link to={this.props.auth ? '/surveys' : '/'}
+                          className="left brand-logo">
+                        Emaily</Link>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="sass.html">Login with Google</a></li>
-                        {/*<li><a href="badges.html">Components</a></li>
-                            <li><a href="collapsible.html">JavaScript</a></li>*/}
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
@@ -17,4 +34,8 @@ class Header extends Component{
     }
 }
 
-export default Header;
+function mapStateToProps(state){
+    return {auth: state.auth};
+}
+
+export default connect(mapStateToProps)(Header);
