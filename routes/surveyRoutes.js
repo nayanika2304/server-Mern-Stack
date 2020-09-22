d597f312b0c2331f475bcc3c0f69201318c88bda
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-09-22 10:00:50 -0400

    updates

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -49,7 +49,7 @@ module.exports = (app) =>{
 
     app.post('/api/surveys/webhooks',(req,res) =>{
         const p = new Path('/api/surveys/:surveyId/:choice');
-         _.chain(req.body)
+        _.chain(req.body)
             .map(({email,url}) =>{
             const match = p.test(new URL(url).pathname)
             if(match){
